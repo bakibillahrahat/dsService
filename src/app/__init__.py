d8e0e7703d76ce3b608c85e6f2586cd56d85ddbc
MD. Bakibillah Rahat <bakibillahrahat@protonmail.com>
2025-01-02 13:16:57 +0600

    add new feature

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,27 +1,32 @@
 import json
 from flask import Flask
 from flask import request, jsonify
-from service.messageService import MessageService
-from kafka3 import KafkaProducer
-
+from kafka import KafkaProducer
+from .service.messageService import MessageService
+import os
+import jsonpickle
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+kafka_host = os.getenv('KAFKA_HOST', '89.116.167.32')
+kafka_port = os.getenv('KAFKA_PORT', '9092')
+kafka_bootstrap_servers = f'{kafka_host}:{kafka_port}'
+print(f'Kafka bootstrap servers: {kafka_bootstrap_servers}')
+print("\n")
+producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message', methods=['POST'])
-
 def handle_message():
     message = request.json.get('message')
     result = messageService.process_message(message)
-    serialized_result = result.json()
-
-    producer.send('expense_service', serialized_result)
-
-    
-    return jsonify(result)
+    if result is not None:
+        serialized_result = result.serialize()
+        producer.send('expense_service', serialized_result)
+        return jsonify(serialized_result)
+    else:
+        return jsonify({'error': 'Invalid message format'}), 400
 
 @app.route('/', methods=['GET'])
 def handle_get():
